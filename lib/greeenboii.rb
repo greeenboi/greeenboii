2fe30905d9ab830280ae76086821c3c869d0ae95
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-02-23 19:37:09 +0530

    basic frame

--- lib/greeenboii.rb
+++ lib/greeenboii.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 require_relative "greeenboii/version"
-require_relative "greeenboii/greeenboii"
+require 'cli/ui'
 
 module Greeenboii
   class Error < StandardError; end
-  # Your code goes here...
+  class Main
+    CLI::UI::StdoutRouter.enable
+    CLI::UI::Frame.open('Frame 1') do
+      CLI::UI.frame_style = :bracket
+      CLI::UI::Frame.open('Frame 2') { puts "inside frame 2" }
+      puts "inside frame 1"
+    end
+  end
 end
