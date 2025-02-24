3675e6024525ab2859e9004c1e71d9570a8664fd
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-02-24 11:21:29 +0530

    Sorbet & tapioca tc

--- sorbet/tapioca/require.rb
+++ sorbet/tapioca/require.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 # Add your extra requires here (`bin/tapioca require` can be used to bootstrap this list)
-require 'cli/ui'
+require "cli/ui"
 require "date"
 require "console_table"
 require "nokogiri"
